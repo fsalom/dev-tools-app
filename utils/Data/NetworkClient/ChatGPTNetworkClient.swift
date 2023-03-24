8b22b014562e6534e60b9d53c89d3acf58b1bd9d
Fernando Salom <fer@rudo.es>
2023-03-24 19:15:30 +0100

    Remove key

--- utils/Data/NetworkClient/ChatGPTNetworkClient.swift
+++ utils/Data/NetworkClient/ChatGPTNetworkClient.swift
@@ -24,35 +24,33 @@ final class ChatGPTNetworkClient: ChatGPTNetworkClientProtocol {
 
     func send(this prompt: String) async throws -> String {
         let openAIEndpoint = "https://api.openai.com/v1/chat/completions"
-        let openAIKey = "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"
+        let openAIKey = ""
 
         let headers = [
             "Content-Type": "application/json",
             "Authorization": "Bearer \(openAIKey)"
         ]
 
-        let parameters: [String: Any] = [
-            "model": "gpt-3.5-turbo",
-            "messages": [MessageGPT(role: "user", content: prompt)],
-        ]
-
         guard let url = URL(string: openAIEndpoint) else {
             throw ChatError.invalidURL
         }
 
+        let parameters = ["model": "gpt-3.5-turbo",
+                          "messages": [["role": "user",
+                                        "content": prompt]]] as [String : Any]
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.allHTTPHeaderFields = headers
-        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
-
+        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
         let (data, response) = try await URLSession.shared.data(for: request)
-        print(response)
-        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-           let choices = json["choices"] as? [[String: Any]],
-           let text = choices.first?["text"] as? String {
-            return text
-        } else {
+
+        print(data)
+        guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
             throw ChatError.fail
         }
+        print(response)
+        let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+        let completions = (json?["choices"] as? [[String: Any]])?.first?["text"] as? String ?? ""
+        return completions
     }
 }
